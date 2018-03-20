efb8e595cb56f731de58fb4bc1db07f21c16f36f
matbakutis <31048002+matbakutis@users.noreply.github.com>
2018-03-20 09:14:39 -0700

    finished Lab

---
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,17 @@
+INSERT INTO USERS
+  (USER_NAME, FIRST_NAME, LAST_NAME)
+VALUES
+  ('user from seeds', 'Ima', 'User'),
+  ('another user', 'Ura', 'User'),
+  ('someone', 'Heeza', 'User'),
+  ('else', 'Sheeza', 'User');
+
+INSERT INTO SONGS
+  (TITLE, LENGTH)
+VALUES
+  ('song1', '00:05:06'),
+  ('song2', '00:04:06'),
+  ('song3', '00:04:36'),
+  ('song4', '00:02:22'),
+  ('song5', '00:07:56'),
+  ('song6', '00:03:30');
